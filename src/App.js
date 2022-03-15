4f0fcc92b35e74412d49744a874067d08b3ff94c
The Duong <daoduong.the@gmail.com>
2022-03-15 08:19:58 +0700

    foldel fe and example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
+import TodoFeature from "./features/Todo";
 
 function App() {
   const name = "Dương";
@@ -14,46 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Thế Dương 09 </p>
-        Learn React
-        <p>
-          Xin chào {name} - {age} - {isMale ? "Male" : "Female"}
-        </p>
-        {/* When content small */}
-        {isMale ? <p>Male</p> : <p>Femele</p>}
-        {/* when Content large */}
-        {isMale && <p>Ối dồi ôi</p>}
-        {!isMale && <p>05165146514654135113</p>}
-        {isMale && (
-          <div>
-            <p>Ối dồi ôi</p>
-            <p>Ối dồi ôi</p>
-          </div>
-        )}
-        {isMale && (
-          <React.Fragment>
-            <p>Thẻ 1</p>
-            <p>Thẻ 2</p>
-            <p>Thẻ 3</p>
-          </React.Fragment>
-        )}
-        {isMale && (
-          <>
-            <p>{student.name}</p>
-            <p>Thẻ 5</p>
-            <p>Thẻ 6</p>
-          </>
-        )}
-        <ul>
-          {colorList.map((color) => (
-            <li key={color} style={{ color }}>
-              {color}
-            </li>
-          ))}
-        </ul>
-      </header>
+      <TodoFeature />
     </div>
   );
 }
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import TodoList from "./components/TodoList";
 
-TodoFeature.propTypes = {
-    
-};
+TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  const todoList = [
+    {
+      id: 1,
+      title: "Eat",
+    },
+    {
+      id: 2,
+      title: "Sleep",
+    },
+    {
+      id: 3,
+      title: "Code",
+    },
+    {
+      id: 4,
+      title: "Learn",
+    },
+  ];
+  return <div>
+      <h3>Todo List</h3>
+      <TodoList todoList={todoList}/>
+  </div>;
 }
 
 export default TodoFeature;
